7807c64669cf30277ac34b3c8aa9bc72d2541c50
yamamoto <c0a211351b@edu.teu.ac.jp>
2022-11-22 16:06:47 +0900

    時間測定修正

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,5 +1,5 @@
 from random import choice
-import time
+import datetime
 
 def shutudai():
     que_dict = {}
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     question, f_ans = shutudai()
     print("問題：")
     print(question)
-    start_time = time.time()
+    start_time = datetime.datetime.now()
     ans = input("答えるんだ：")
-    end_time = time.time() - start_time
+    end_time = datetime.datetime.now()
     if ans in f_ans:
         print("正解！！！")
     else:
         print("出直してこい")
-    print(f"所要時間：{end_time}")
+    print(f"所要時間：{(start_time-end_time).seconds}")
