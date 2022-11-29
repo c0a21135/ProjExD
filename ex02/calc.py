092c549ce3731047f5137110e70fb41568976e7b
yamamoto <c0a211351b@edu.teu.ac.jp>
2022-11-29 15:55:43 +0900

    記号連続で入力できないように

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,23 +5,40 @@ root = tk.Tk()
 root.title("tk")
 root.geometry("500x1000")
 
+mode = "DEC" #n進数判定
+ch_ope = False #数字以外の入力識別
+operators = ["+", "-","*", "/", "C", "=", "DEC","BIN"]
 def button_click(event):
+    global ch_ope, mode
     btn = event.widget
     txt = btn["text"]
-    if not txt == "=":
-        entry.insert(tk.END, txt)
-    else:
+
+    if not str(txt) in operators: #数字の入力受付
+        ch_ope = False
+
+    if txt == "=":
         siki = entry.get()
         res = eval(siki)
         entry.delete(0, tk.END)
         entry.insert(tk.END, res)
+    
+    elif txt == "C":
+        entry.delete(0, tk.END)
+
+
+    else:
+        if ch_ope == False:
+            entry.insert(tk.END, txt)
+            if str(txt) in operators: #記号の入力判定
+                ch_ope = True 
+    
+
 
 entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(columnspan=3)
 
 
 #数字ボタンの実装
-
 r, c = 4, 0
 for i in range(9, -1, -1):
     button = tk.Button(root, text=i, font=("", 30), width=4, height=2)
@@ -34,7 +51,7 @@ for i in range(9, -1, -1):
 
 
 
-operators = ["+", "-","*", "/", "C", "="]
+
 for ope in operators:
     button = tk.Button(root, text=ope, font=("", 30), width=4, height=2)
     button.bind("<ButtonRelease>", button_click)
@@ -43,6 +60,7 @@ for ope in operators:
     if c%3 == 0:
         r += 1
         c = 0
+
 
 root.mainloop()
 
