35d20a69bfe9716948345d43b12ebad82f7f2e65
yamamoto <c0a211351b@edu.teu.ac.jp>
2022-11-29 14:51:32 +0900

    練習7

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,10 +8,16 @@ root.geometry("300x500")
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
-    tkm.showinfo(txt, f"[{txt}]ボタンがクリックされました")
+    if not txt == "=":
+        entry.insert(tk.END, txt)
+    else:
+        siki = entry.get()
+        res = eval(siki)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
 
 entry = tk.Entry(root, justify="right", width=10, font=("",40))
-entry.grid(row = 0, column = 0, columnspan=3)
+entry.grid(columnspan=4)
 
 for i in range(9, -1, -1):
     button = tk.Button(root, text=9-i, font=("", 30), width=4, height=2)
