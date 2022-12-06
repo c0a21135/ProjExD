2d7794c1b638e4eb19738ccd550060562f85f15e
yamamoto <c0a211351b@edu.teu.ac.jp>
2022-12-06 14:32:30 +0900

    練習6

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,7 +3,10 @@ import tkinter as tk
 def key_down(event):
     global key
     key = event.keysym
-
+    
+def key_up(event):
+    global key
+    key = ""
 
 root = tk.Tk()
 root.title("迷えるこうかとん")
@@ -15,5 +18,6 @@ canvas.create_image(cx, cy, image=image)
 canvas.pack()
 key = ""
 root.bind("<KeyPress>", key_down)
+root.bind("<KeyRelease", key_up)
 
 root.mainloop()
