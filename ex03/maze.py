403dcfc259ff93211693b5fa650e23560e15e2c1
yamamoto <c0a211351b@edu.teu.ac.jp>
2022-12-06 18:33:12 +0900

    最終更新

--- ex03/maze.py
+++ ex03/maze.py
@@ -48,33 +48,31 @@ def main_proc():
 def ch_game_flag(event): #ゲームの進行を管理する関数
     global game_flag, key
     key = event.keysym
-    # ゲームの開始
-    if game_flag == False and key == "s":
+    
+    if game_flag == False and key == "s":# ゲームの開始
         game_flag = True
         time_count() #タイマーの開始
-    # ゲームの一時停止
-    if key == "f":
+    if key == "f":    # ゲームの一時停止
         game_flag = False
         time_count() #タイマーの停止
-    # ゲームの初期化
-    if key == "r":
+    if key == "r":    # ゲームの初期化
         game_flag = False
         init()
 
 def time_count(): #時間の計測と表示
     global sec, ms, jid
-    if game_flag:
+    if game_flag: #ゲーム実行中ならば
         ms += 1
         if ms == 10:
             ms = 0
             sec += 1
         jid = root.after(100, time_count)
-    else:
+    else: #ゲームが停止した際にカウントを止める
         root.after_cancel(jid)
     label["text"] = f"{sec}.{ms}"
     label.pack()
 
-def init():
+def init(): #ゲームの初期化
     global mx, my, sec, ms, image
     sec, ms = 0, 0
     image = tk.PhotoImage(file="0.png")
@@ -86,25 +84,26 @@ def init():
     time_count()
 
 if __name__ == "__main__":
-    game_flag = False
+    game_flag = False #ゲームの進行を管理する
+    sec, ms = 0, 0 #秒数を管理する
+    mx, my = 1, 1 #こうかとんの座標をリストのインデントで管理する
+    cx, cy = mx*100+50, my*100+50 #こうかとんの座標をtk上の座標で管理する
+    key = "" #入力されたキーを管理する
+
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(width=1500, height=900, bg="black")
     image = tk.PhotoImage(file="0.png")
-    mx, my = 1, 1
-    cx, cy = mx*100+50, my*100+50
     canvas.pack()
     maze_list = mm.make_maze(15, 9)
     mm.show_maze(canvas, maze_list)
     canvas.create_image(cx, cy, image=image, tag="kokaton")
 
-    key = ""
     root.bind("<KeyPress>", key_down)
-    root.bind("<KeyPress>", ch_game_flag)
+    root.bind("<KeyPress>", ch_game_flag) #ゲームフラッグを管理
     root.bind("<KeyRelease>", key_up)
     root.after(100, main_proc)
     label = tk.Label(root, font=("", 80))
-    sec, ms = 0, 0
     label["text"] = f"{sec}.{ms}"
     label.pack()
 
