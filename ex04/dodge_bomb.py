9db4923700c85e0ca18d45617fbb470c72a9ab28
yamamoto <c0a211351b@edu.teu.ac.jp>
2022-12-13 14:48:11 +0900

    練習5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,19 +1,29 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
-    scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_sfc = pg.display.set_mode((1600, 900)) #スクリーン生成
+    scrn_rct = scrn_sfc.get_rect()
     
     pgbg_sfc = pg.image.load("fg/pg_bg.jpg") #背景作成
     pgbg_rct = pgbg_sfc.get_rect()
 
-    tori_sfc = pg.image.load("fg/6.png")
+    tori_sfc = pg.image.load("fg/6.png") #鳥作成
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
     scrn_sfc.blit(tori_sfc, tori_rct)
+
+    bomb_sfc = pg.Surface((20, 20)) #爆弾生成
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 20)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct)
 
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct)
@@ -27,7 +37,9 @@ def main():
         if key_dict[pg.K_LEFT]: tori_rct.centerx -= 1
         if key_dict[pg.K_RIGHT]: tori_rct.centerx += 1
         scrn_sfc.blit(tori_sfc, tori_rct)
-        
+
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
+
         pg.display.update()
 
         clock.tick(1000)
